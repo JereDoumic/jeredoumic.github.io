9eef5c45567463e027c53455fabeb48b26255561
JereDoumic <jere.dou@hotmail.com>
2024-01-06 12:48:29 -0300

    last up

--- Js/func.js
+++ Js/func.js
@@ -1,18 +1,8 @@
 
 function changeBackground(){
-    /*if(document.getElementById("saludar").innerHTML == "hola"){
-        document.getElementById("saludar").innerHTML = "chau";
-    } else {  
-        document.getElementById("saludar").innerHTML = "hola";
-    }*/
-    /*console.log("apretado");
-    var body = document.getElementById("body");
- 
-    body.style.backgroundColor = "dark";*/
 
     var body = document.getElementById("body");
     body.classList.toggle("body-2");
-    
 
 }
 
@@ -20,12 +10,19 @@ function openCV(){
     "window.open(‘CV\Jeremias Doumic CV.pdf’)";
 }
 
-function getForm(){
-    var nombre = document.getElementById("nombre").value;
-    var mail = document.getElementById("mail").value;
-    var text = document.getElementById("text").value;
 
-    console.log(nombre);
-    console.log(mail);
-    console.log(text);
-}
+
+function copyToClipboard() {
+    var emailElement = document.getElementById('email');
+    var emailText = emailElement.innerText;
+
+    var inputElement = document.createElement('input');
+    inputElement.value = emailText;
+
+    document.body.appendChild(inputElement);
+
+    inputElement.select();
+    document.execCommand('copy');
+
+    document.body.removeChild(inputElement);
+  }
