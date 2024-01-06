eb6f9aeabd2811cb920bb18b9ea2b4567b488ddf
JereDoumic <jere.dou@hotmail.com>
2024-01-06 12:54:17 -0300

    last update

--- Js/func.js
+++ Js/func.js
@@ -9,20 +9,3 @@ function changeBackground(){
 function openCV(){
     "window.open(‘CV\Jeremias Doumic CV.pdf’)";
 }
-
-
-
-function copyToClipboard() {
-    var emailElement = document.getElementById('email');
-    var emailText = emailElement.innerText;
-
-    var inputElement = document.createElement('input');
-    inputElement.value = emailText;
-
-    document.body.appendChild(inputElement);
-
-    inputElement.select();
-    document.execCommand('copy');
-
-    document.body.removeChild(inputElement);
-  }
